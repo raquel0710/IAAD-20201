89ad8cafd95e1e5c9f8fdfed466d6721a7fcf13f
Débora Silva <50027549+raquel0710@users.noreply.github.com>
2020-03-10 21:48:35 -0300

    Update ex5d.sql

--- ex5d.sql
+++ ex5d.sql
@@ -1,3 +1,5 @@
+# Número do pedido, nome do cliente e nome do vendedor;
+
 SELECT p.numero_pedido, c.nome, v.nome_vendedor FROM pedidos p
 JOIN clientes c ON (p.codigo_cliente = c.codigo_cliente)
 JOIN vendedores v ON (v.codigo_vendedor = p.codigo_vendedor)
